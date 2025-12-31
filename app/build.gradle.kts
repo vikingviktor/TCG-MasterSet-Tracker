076d3b943eb787e8cc245bf2773804b5112aa4cb
Victor Rubio Alonso <victor.rubio@securitasdirect.es>
2025-12-31 09:25:18 +0100

    version bundle 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mastersettracker.pokemon"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
