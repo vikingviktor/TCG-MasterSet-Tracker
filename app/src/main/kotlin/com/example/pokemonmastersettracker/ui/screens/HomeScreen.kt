94b7a788e276d532fe5c071f293165d5560436b0
Victor Rubio Alonso <victor.rubio@securitasdirect.es>
2026-01-05 11:08:17 +0100

    version bundle 30

--- app/src/main/kotlin/com/example/pokemonmastersettracker/ui/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/example/pokemonmastersettracker/ui/screens/HomeScreen.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
