0b64bf22d0e45576a8726629866d281783d89de8
Victor Rubio Alonso <victor.rubio@securitasdirect.es>
2026-01-09 08:58:03 +0100

    upgraded to version 53

--- clean_database.sql
+++ clean_database.sql
@@ -3,12 +3,12 @@
 
 -- Remove all user-specific data (keep only Pokemon and Cards data)
 DELETE FROM favorite_pokemon;
-DELETE FROM wishlist_card;
+DELETE FROM wishlist_cards;
 DELETE FROM user_cards;
 
 -- Verify tables are empty
 SELECT COUNT(*) as favorite_count FROM favorite_pokemon;
-SELECT COUNT(*) as wishlist_count FROM wishlist_card;
+SELECT COUNT(*) as wishlist_count FROM wishlist_cards;
 SELECT COUNT(*) as user_cards_count FROM user_cards;
 
 -- Verify the data we want to keep is still there
