10e70d0c5d823a78e5410454ea1d3693a8539bb1
Victor Rubio Alonso <victor.rubio@securitasdirect.es>
2026-01-19 17:08:01 +0100

    upgraded to version 64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mastersettracker.pokemon"
         minSdk = 24
         targetSdk = 35
-        versionCode = 63
-        versionName = "2.3.0"
+        versionCode = 64
+        versionName = "2.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
