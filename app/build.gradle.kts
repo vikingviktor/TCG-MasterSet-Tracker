debc5c66d0637df7278ad078d7bc002b88b4e511
Victor Rubio Alonso <victor.rubio@securitasdirect.es>
2026-01-21 08:40:01 +0100

    upgraded to version 75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mastersettracker.pokemon"
         minSdk = 24
         targetSdk = 35
-        versionCode = 74
-        versionName = "2.5.2"
+        versionCode = 75
+        versionName = "2.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/kotlin/com/example/pokemonmastersettracker/data/database/PokemonTrackerDatabase.kt
+++ app/src/main/kotlin/com/example/pokemonmastersettracker/data/database/PokemonTrackerDatabase.kt
@@ -22,7 +22,7 @@ import com.example.pokemonmastersettracker.data.converters.TypeConverters as Roo
         User::class,
         Pokemon::class
     ],
-    version = 7,
+    version = 8,
     exportSchema = false
 )
 @TypeConverters(RoomTypeConverters::class)
